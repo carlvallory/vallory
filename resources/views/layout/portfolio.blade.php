70a8af7db48f6232c9ff899d14614d66ebadcbc0
Carlos Vallory <carlvallory@gmail.com>
2021-02-03 12:05:58 -0300

    Env Var

--- resources/views/layout/portfolio.blade.php
+++ resources/views/layout/portfolio.blade.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Stylish Portfolio - Start Bootstrap Template</title>
+  <title>{{ env('APP_NAME') }} Portfolio</title>
 
   <!-- Bootstrap Core CSS -->
   <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
--- resources/views/resume/index.blade.php
+++ resources/views/resume/index.blade.php
@@ -12,8 +12,8 @@
                         <span class="text-primary">Vallory</span>
                     </h1>
                     <div class="subheading mb-5">
-                        3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·
-                        <a href="mailto:name@email.com">name@email.com</a>
+                        {{env('STREET_ADDRESS')}} · {{env('PHONE_NUMBER')}} ·
+                        <a href="mailto:{{env('GMAIL_LINK')}}">{{env('GMAIL_LINK')}}</a>
                     </div>
                     <p class="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
                     <div class="social-icons">
